d6aaf5d0a725fdd9c1230257c09e5848a9320ef0
daniel <daniel.varela@udc.es>
2020-06-18 17:06:36 +0200

    test pages

--- publish.el
+++ publish.el
@@ -8,16 +8,24 @@
          :recursive t
          :publishing-function org-html-publish-to-html
          :auto-sitemap t
-         :sitemap-title "The Blog"
-         :sitemap-filename "index.org"
+         :sitemap-title ""
+         :sitemap-filename "sitemap.org"
          :sitemap-style list
          :author "Daniel Varela"
          :email "daniel.varela@biochemistry.lu.se"
          :with-creator t)
+        ("index"
+         :base-directory "index"
+         :base-extention "org"
+         :section-numbers nil
+         :with-toc nil
+         :html-postamble t
+         :publishing-directory "./"
+         :publishing-function org-html-publish-to-html)
         ("css"
           :base-directory "css/"
           :base-extension "css"
           :publishing-directory "css/"
           :publishing-function org-publish-attachment
           :recursive t)
-        ("all" :components ("posts" "css"))))
+        ("all" :components ("posts" "index" "css"))))
